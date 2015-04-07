615d2a1bbaa03a7de43994d70b05471f2dc3b20a
Tayphoon <tayphoon.company@gmail.com>
2015-04-07 15:09:08 +0300

    Split model to edit and display model, add check changes

---
+++ IQ300/Tasks/Model/ManagedTodoListModel.h
@@ -0,0 +1,13 @@
+//
+//  ManagedTodoListModel.h
+//  IQ300
+//
+//  Created by Tayphoon on 07.04.15.
+//  Copyright (c) 2015 Tayphoon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ManagedTodoListModel : NSObject
+
+@end
---
+++ IQ300/Tasks/Model/ManagedTodoListModel.m
@@ -0,0 +1,13 @@
+//
+//  ManagedTodoListModel.m
+//  IQ300
+//
+//  Created by Tayphoon on 07.04.15.
+//  Copyright (c) 2015 Tayphoon. All rights reserved.
+//
+
+#import "ManagedTodoListModel.h"
+
+@implementation ManagedTodoListModel
+
+@end
